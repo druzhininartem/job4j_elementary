c532e770e56f99e590a84181ceba9a6ab90bac2b
druzhininartem <artemdruzhinin1985ad@gmail.com>
2024-01-12 13:45:58 +0400

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Artem Druzhinin");
         System.out.println("19.11.1985");
     }
