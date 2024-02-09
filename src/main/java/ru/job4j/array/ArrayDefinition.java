29d1dba453c6c9c3dc70a09493a78a0817820bef
druzhininartem <artemdruzhinin1985ad@gmail.com>
2024-02-09 16:45:20 +0400

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,8 +5,19 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Artem";
+        names[1] = "Maria";
+        names[2] = "Arina";
+        names[3] = "Alexandra";
         System.out.println("Размер short[] ages равен: " + ages.length);
         System.out.println("Размер String[] surnames равен: " + surnames.length);
         System.out.println("Размер float[] prices равен: " + prices.length);
+        System.out.println("-------------------------------");
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 }
