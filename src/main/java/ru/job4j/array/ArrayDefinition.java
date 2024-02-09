6f1dbb97edcd3db3bee8e9bec2c205e97764c6fb
druzhininartem <artemdruzhinin1985ad@gmail.com>
2024-02-09 16:26:49 +0400

    6.0.2. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,8 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        System.out.println("Размер short[] ages равен: " + ages.length);
+        System.out.println("Размер String[] surnames равен: " + surnames.length);
+        System.out.println("Размер float[] prices равен: " + prices.length);
     }
 }
